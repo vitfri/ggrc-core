edc044771081065ba911a84c887af4455c9bf445
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2017-04-28 19:21:56 +0300

    Fix running tests

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -6,8 +6,12 @@ a new instance of your application. This runs before any WSGI applications
 specified in app.yaml are loaded.
 """
 
+import os
+
 from google.appengine.ext import vendor
 
 # Third-party libraries are stored in "packages", vendoring will make
 # sure that they are importable by the application.
-vendor.add('packages')
+dir_path = os.path.dirname(os.path.realpath(__file__))
+packages_path = os.path.join(dir_path, "packages")
+vendor.add(packages_path)
